ec452c958b16468ad63fffe39aab881c49c3cb0b
kakcalu13 <kevin.a.araujo@gmail.com>
2024-11-01 09:37:43 -0400

    disabled sound

--- src/sound.h
+++ src/sound.h
@@ -70,14 +70,14 @@ byte volumeTest[] = {
 };
 void beep(float note, float duration = 50, int pause = 0, byte repeat = 1) {
   if (soundState) {
-    for (byte r = 0; r < repeat; r++) {
-      if (note > 0) {
-        tone(BUZZER, BASE_PITCH * pow(1.05946, note), duration, buzzerVolume / amplifierFactor);
-      } else {
-        delay(duration);
-      }
-      delay(pause);
-    }
+//    for (byte r = 0; r < repeat; r++) {
+//      if (note > 0) {
+//        tone(BUZZER, BASE_PITCH * pow(1.05946, note), duration, buzzerVolume / amplifierFactor);
+//      } else {
+//        delay(duration);
+//      }
+//      delay(pause);
+//    }
   }
 }
 
